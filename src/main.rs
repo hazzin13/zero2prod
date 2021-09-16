3be92ec21797f3ee2e022e1d35bc6be9f1fb7427
hazzin <hazzin13@gmail.com>
2021-09-16 19:53:23 +0200

    Change to CI/CD pipeline

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // test
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
